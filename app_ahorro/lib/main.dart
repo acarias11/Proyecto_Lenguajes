6a5f0d0b15918505be037284cc6002ee57646049
DarlanP17 <darlan.perdomo@unah.hn>
2024-08-01 18:43:06 -0600

    Prueba de conexion a la base de Supabase

--- app_ahorro/lib/main.dart
+++ app_ahorro/lib/main.dart
@@ -1,9 +1,18 @@
+import 'dart:js_interop';
+
 import 'package:app_ahorro/historial_page.dart';
 import 'package:app_ahorro/home_page.dart';
 import 'package:app_ahorro/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://aycfiajfrsjrkxkmxqgh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5Y2ZpYWpmcnNqcmt4a214cWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0ODU4MTAsImV4cCI6MjAzODA2MTgxMH0.__3i5GG-uXwvAhnrcDrDGEZAf9Lumy-M3Ewhh60YMjs',
+  );
   runApp(const MainApp());
 }
 
@@ -41,6 +50,32 @@ class MainApp extends StatelessWidget {
                 ],
               ),
             ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: (){
+                showDialog(
+                  context: context, 
+                  builder: (context) {
+                    return SimpleDialog(
+                      title: const Text('Datos'),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                      children: [
+                        TextFormField(
+                          onFieldSubmitted: (value) async {
+                            await Supabase.instance.client.from('categorias').insert({'nombre': value});
+                          },
+                        ),
+                        TextFormField(
+                          onFieldSubmitted: (value) async {
+                            await Supabase.instance.client.from('categorias').insert({'tipo': value});
+                          },
+                        )
+                      ],
+                    );
+                  },
+                  );
+              },
+              child: const Icon(Icons.add),
+              ),
           ),
         );
       },
